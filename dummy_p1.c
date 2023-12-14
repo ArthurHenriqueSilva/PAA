b15fb79c5e58ab616ba89636933763a73c522bbe
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2023-12-14 18:55:14 -0300

    tentando do zero

--- dummy_p1.c
+++ dummy_p1.c
@@ -59,42 +59,6 @@ Container *create_vector_containers(int quant, FILE *input){
     return cont;
 }
 
-Erro *aux_survey_container(Container container, Container *containers, int size_containers){
-    Erro *erros = (Erro *)malloc(size_containers * sizeof(Container));
-    int erro_index = 0;
-    for(int i = 0; i < size_containers; i++){
-        if(compare_strings(container.code, containers[i].code)){
-            if (!(compare_strings(container.cnpj, containers[i].cnpj))){
-                erros[erro_index].precedence = 0;
-                erros[erro_index].code = container.code;
-                erros[erro_index].cnpj1 = container.cnpj;
-                erros[erro_index].cnpj2 = containers[i].cnpj;
-                erro_index++;
-                printf("%s: %s <-> %s\n", container.code, container.cnpj, containers[i].cnpj);
-                continue;
-            }
-            int diff = container.weight - containers[i].weight;
-            float percent = (float)diff / containers[i].weight;
-            percent *= 100;
-            percent = my_round(percent);
-            if (percent > 10){
-                erros[erro_index].code = 1;
-                erros[erro_index].code = container.code;
-                erros[erro_index].diff = diff;
-                erros[erro_index].percent = percent;
-
-            } printf("%s: %dkg (%.0f%%)\n", container.code, diff, percent);
-
-        }
-
-    }
-}
-
-void survey_containers(Container *containers_2, int size_containers_2, Container *containers_1, int size_containers_1){
-    for(int i = 0; i < size_containers_2; i++){
-       aux_survey_container(containers_2[i], containers_1, size_containers_1); 
-    }
-}
 
 int main(int argc, char *argv[]){
     FILE *input = fopen(argv[1], "r");
@@ -102,11 +66,10 @@ int main(int argc, char *argv[]){
     int number_lines_1 = read_number(input);
     
     Container *containers = create_vector_containers(number_lines_1, input);
-    // printContainers(containers, number_lines_1);
     int number_lines_2 = read_number(input);
     Container *containers_2 = create_vector_containers(number_lines_2, input);
+    // printContainers(containers, number_lines_1);
     // printContainers(containers_2, number_lines_2);
-    survey_containers(containers_2, number_lines_2, containers, number_lines_1);
 
     free(containers);
     fclose(input);
