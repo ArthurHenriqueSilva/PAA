cc939717b82505ca10fbcc9c7e075bf08bbf2db7
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2023-12-16 17:11:26 -0300

    Mudanca da classe error para 2 classes separadas

--- dummy_p1.c
+++ dummy_p1.c
@@ -8,13 +8,17 @@ typedef struct {
 } Container;
 
 typedef struct {
-    int precedence;
     char code[12];
     char cnpj1[18];
     char cnpj2[18];
+} CNPJ_Error;
+
+typedef struct {
+    char code[12];
     int diff;
-    float percent;
-} Error;
+    int percent;
+
+}WEIGHT_Error;
 
 int my_round(float x) {
     return (int)(x + 0.5);
@@ -57,52 +61,7 @@ Container *create_vector_containers(int quant, FILE *input) {
     return cont;
 }
 
-Error define_Error(Container container1, Container container2) {
-    Error this_error;
-    copy_string(this_error.code, container1.code);
-    if ((this_error.precedence = compare_strings(container1.cnpj, container2.cnpj)) == 0) {
-        copy_string(this_error.cnpj1, container1.cnpj);
-        copy_string(this_error.cnpj2, container2.cnpj);
-    } else {
-        int diff = container1.weight - container2.weight;
-        this_error.diff = diff < 0 ? -diff : diff;
-        this_error.percent = my_round(((float)this_error.diff / (float)container1.weight) * 100);
-    }
 
-    return this_error;
-}
-
-Error *find_sameCode_containers(Container *container1, int nl1, Container *container2, int nl2, int *num_errors) {
-    Error *errors = (Error *)malloc(nl1 * sizeof(Error));
-    int error_index = 0;
-    for (int i = 0; i < nl1; i++) {
-        for (int j = 0; j < nl2; j++) {
-            if (compare_strings(container1[i].code, container2[j].code)) {
-                errors[error_index++] = define_Error(container1[i], container2[j]);
-            }
-        }
-    }
-    *num_errors = error_index;
-    return errors;
-}
-
-void printErrors(Error *errors, int num_errors) {
-    for (int i = 0; i < num_errors; i++) {
-        printf("Error %d: Code: %s Precedence: %d ", i + 1, errors[i].code, errors[i].precedence);
-
-        if (errors[i].precedence == 1) {
-            printf("Diff: %d Percent: %0.f%%", errors[i].diff, errors[i].percent);
-        } else {
-            printf("CNPJ1: %s CNPJ2: %s ",
-                   errors[i].cnpj1, errors[i].cnpj2);
-        }
-        printf("\n");
-    }
-}
-
-void freeErrors(Error *errors) {
-    free(errors);
-}
 
 
 int main(int argc, char *argv[]) {
