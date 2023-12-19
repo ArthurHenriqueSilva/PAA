fc56b7507fc7608513c81f358370055a584e3e00
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2023-12-19 19:53:10 -0300

    solution with mergeSort and BinarySearch returning sorted weight errors

--- arthurlima_202000012551_porto.c
+++ arthurlima_202000012551_porto.c
@@ -10,6 +10,22 @@ typedef struct {
     int weight;
 } Container;
 
+typedef struct {
+    char code[CODE_SIZE];
+    int diff;
+    int percent;
+}Weight_error;
+
+int my_round(float x) {
+    return (int)(x + 0.5);
+}
+
+char *copy_string(char *dest, char *src) {
+    char *original_dest = dest;
+    while ((*dest++ = *src++) != '\0');
+    return original_dest;
+}
+
 int compare_strings(const char *s1, const char *s2) {
     while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
         s1++;
@@ -127,6 +143,62 @@ void mergeSort(Container *arr, int left, int right) {
     }
 }
 
+void merge2(Weight_error *arr, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    Weight_error L[n1], R[n2];
+
+    
+    for (i = 0; i < n1; i++) {
+        L[i] = arr[left + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (L[i].percent <= R[j].percent) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort2(Weight_error *arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort2(arr, left, mid);
+        mergeSort2(arr, mid + 1, right);
+        merge2(arr, left, mid, right);
+    }
+}
+
+
+
 int binarySearch(Container *c, int low, int high, const char *code) {
     while (low <= high) {
         int mid = low + (high - low) / 2;
@@ -141,20 +213,34 @@ int binarySearch(Container *c, int low, int high, const char *code) {
     return -1;
 }
 
-void search_container(Container *c1, int n1, Container *c2, int n2) {
-    for (int i = 0; i < n1; i++) {
-        const char *code = c1[i].code;
-        int index = binarySearch(c2, 0, n2 - 1, code);
-        if (index != -1)
-            printf("Found: %s == %s\n", code, c2[index].code);
-        else
-            printf("Not found: %s\n", code);
-    }
+
+int define_error(Container c1, Container c2, int *percent, int *diff){
+    if(compare_strings(c1.cnpj, c2.cnpj) != 0) return 0;
+    *diff = c1.weight - c2.weight;
+    *diff = *diff < 0 ? (-*diff):*diff;
+    *percent = my_round(((float)*diff / (float)c1.weight) * 100);
+    if (*percent > 10) return 1;
+    return 2;
 }
 
-void printContainers(Container *cont, int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%s %s %d\n", cont[i].code, cont[i].cnpj, cont[i].weight);
+void search_container(Container *c1, int n1, Container *c2, int n2, FILE *output, Weight_error *q) {
+    int percent, diff;
+    int w_index = 0;
+    for (int i = 0; i < n1; i++) {
+        char *code = c1[i].code;
+        int index = binarySearch(c2, 0, n2 - 1, code);
+        if (index != -1){
+            int error_code = define_error(c1[i], c2[index], &percent, &diff);
+            if(error_code == 0) fprintf(output, "%s: %s<->%s\n", c1[i].code, c1[i].cnpj, c2[index].cnpj);
+            if(error_code == 1) {
+                copy_string(q[w_index].code, code);
+                q[w_index].percent = percent;
+                q[w_index].diff = diff;
+                w_index++;
+                // fprintf(output, "%s: %dkg (%d%%)\n", c1[i].code, diff, percent);
+            }
+        }
+
     }
 }
 
@@ -163,21 +249,27 @@ int main(int argc, char *argv[]) {
     FILE *output = fopen(argv[2], "w");
 
     int size_1 = read_number(input);
-    Container container_1[MAX_CONTAINERS];
+    Container container_1[size_1];
     fillContainers(input, container_1, size_1);
 
     int size_2 = read_number(input);
-    Container container_2[MAX_CONTAINERS];
+    Container container_2[size_2];
     fillContainers(input, container_2, size_2);
 
     mergeSort(container_2, 0, size_2 - 1);
 
-    printf("Sorted container_2:\n");
-    printContainers(container_2, size_2);
+    Weight_error w_q[size_2];
+    for (int i = 0; i < size_2; i++) {
+    w_q[i].percent = 0;
+}
 
-    printf("\nSearch results:\n");
-    search_print(container_1, size_1, container_2, size_2);
-
+    search_container(container_1, size_1, container_2, size_2, output, w_q);
+    mergeSort2(w_q, 0, size_2-1);
+    for (int i = 0; i < size_2; i++) {
+        if (w_q[i].percent != 0) {
+            fprintf(output, "%s: %dkg (%d%%)\n", w_q[i].code, w_q[i].diff, w_q[i].percent);
+        }
+    }
     fclose(input);
     fclose(output);
 
