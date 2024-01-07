f966cb1805096312bb2704c8e38a149ba3d86390
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2024-01-07 20:42:16 -0300

    Lp e Hoare feitos. LA tentando

--- dummy_qs.c
+++ dummy_qs.c
@@ -30,13 +30,34 @@ int find_median_3(int x, int y, int z){
     return (x + y + z - min - max);
 }
 
-void fill_vector(FILE *file, int size, int *v){
-    for(int i = 0; i < size; i++) fscanf(file, "%d", &v[i]);
+void fill_vectors(FILE *file, int size, int *v1, int *v2, int *v3, int *v4, int *v5, int *v6){
+    for(int i = 0; i < size; i++) {
+        int value;
+        fscanf(file, "%d", &value);
+        v1[i] = value;
+        v2[i] = value;
+        v3[i] = value;
+        v4[i] = value;
+        v5[i] = value;
+        v6[i] = value;
+        
+    } 
 }
 
+
 void print_vector(int *v, int size){
+    printf("\n");
     for(int i = 0; i < size; i++) printf("%d ", v[i]);
     printf("\n");
+}
+
+void print_vectors(int *v1, int *v2, int *v3, int *v4, int *v5, int *v6, int size){
+    print_vector(v1, size);
+    print_vector(v2, size);
+    print_vector(v3, size);
+    print_vector(v4, size);
+    print_vector(v5, size);
+    print_vector(v6, size);
 }
 
 int lp(int *v, int i, int j, Counters *counters){
@@ -63,6 +84,40 @@ int lm(int *v, int i, int j, Counters *counters) {
 void lm_qs(int *v, int i, int j, Counters *counters) {
 }
 
+int lr(int *v, int i, int j, Counters *counters){
+    swap(&v[j], &v[i+(rand() % (j - i + 1))], counters);
+    return lr(v, i, j, counters);
+}
+
+void lr_qs(int *v, int i, int j, Counters *counters){
+    counters->calls++;
+    if(i < j){
+        int pivo = lr(v, i, j, counters);
+        lr_qs(v, i, pivo, counters);
+        lr_qs(v, pivo + 1, j, counters);
+    }
+}
+
+int hoare(int *v, int i, int j, Counters *counters){
+    int p = v[i], x = i - 1, y = j + 1;
+    while(1){
+        while(v[--y] > p);
+        while(v[++x] < p);
+        if(x < y) swap(&v[x], &v[y], counters);
+        else return y;
+    }
+}
+
+void hoare_qs(int *v, int i, int j, Counters *counters){
+    counters->calls++;
+    if(i < j){
+        int pivo = hoare(v, i, j, counters);
+        hoare_qs(v, i, pivo, counters);
+        hoare_qs(v, pivo + 1, j, counters);
+    }
+}
+
+
 void print_counters(const char *label, Counters counters){
     printf("%s (%d) ", label, counters.swaps + counters.calls);
 }
@@ -72,27 +127,41 @@ int main(int argc, char *argv[]){
     int quant_vector = read_number(input);
 
     for(int i = 0; i < quant_vector; i++) {
-        Counters counters;
-        counters.swaps = 0;
-        counters.calls = 0;
+        Counters lp_counters, lm_counters, lr_counters, hp_counters;
+        lp_counters.swaps = 0;
+        lp_counters.calls = 0;
+        
+        hp_counters.swaps = 0;
+        hp_counters.calls = 0;
+
+        lr_counters.swaps = 0;
+        lr_counters.calls = 0;
 
         int size = read_number(input);
-        int v[size];
-        fill_vector(input, size, v);
+        int v1[size], v2[size], v3[size], v4[size], v5[size], v6[size];
+        fill_vectors(input, size, v1, v2, v3, v4, v5, v6);
+        // print_vectors(v1,v2,v3,v4,v5, v6, size);
 
-        printf("%d: N (%d) ", i, size);
-        // lp Padrão (LP)
-        Counters lp_counters = counters;
-        lp_qs(v, 0, size - 1, &lp_counters);
+        printf("%d: N(%d) ", i, size);
+        // // lp Padrão (LP) - Passa no input de exemplo
+        lp_qs(v1, 0, size - 1, &lp_counters);
         print_counters("LP", lp_counters);
         
-        // LM
-        // Counters lm_counters = counters;
-        // lm_qs(v, 0, size - 1, &lm_counters);
+        // LM - A FAZER
+        // lm_qs(v2, 0, size - 1, &lm_counters);
         // print_counters("LM", lm_counters);
+
+        // LR - Ordena, mas, a contagem est[a errada
+        lr_qs(v3, 0, size - 1, &lr_counters);
+        print_counters("LR", lr_counters);
+
+        // Hoare Padrao(HP) - Passa no input de exemplo
+        hoare_qs(v4, 0, size - 1, &hp_counters);
+        print_counters("HP", hp_counters);
         printf("\n");
 
-        print_vector(v, size);
+
+
     }
 
     return 0;
