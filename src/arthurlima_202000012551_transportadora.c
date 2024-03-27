b70f4a79153569e05b6491dc313f7ab5f61f89cb
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2024-03-26 21:00:16 -0300

    feat: correct value for allocation. Need to format output.

--- src/arthurlima_202000012551_transportadora.c
+++ src/arthurlima_202000012551_transportadora.c
@@ -1,11 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct File {
-    FILE* input;
-    FILE* output;
-} File;
-
 typedef struct Vehicle {
     char plate[7];
     int weight;
@@ -20,29 +15,8 @@ typedef struct Package {
     int allocated;
 } Package;
 
-typedef struct Allocation {
-    float value;
-    int weight;
-    int volume;
-} Allocation;
-
-File openFiles(char* input, char* output) {
-    File file;
-    file.input = fopen(input, "r");
-    file.output = fopen(output, "w");
-    return file;
-}
-
-int max(int a, int b){
-    int out;
-    out = (a>b) ? a:b;
-    return out;
-}
-
-int read_number(FILE *file){
-    int x;
-    fscanf(file, "%d", &x);
-    return x;
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
 Vehicle* create_vehicle(FILE* input) {
@@ -61,7 +35,7 @@ Vehicle* create_vehicle_arr(FILE* input, int quantity) {
 Package* create_package(FILE* input) {
     Package* package = malloc(sizeof(Package));
     fscanf(input, "%s %f %d %d", package->code, &package->value, &package->weight, &package->volume);
-    package->allocated = 1;
+    package->allocated = 0;
     return package;
 }
 
@@ -72,62 +46,85 @@ Package* create_package_arr(FILE* input, int quantity) {
     return ps;
 }
 
-void print_allocation_matrix(int*** matrix, int pn, int WL, int VL) {
-    for (int i = 0; i < pn; i++) {
-        printf("Package %d:\n", i + 1);
-        for (int j = 0; j < WL; j++) {
-            for (int k = 0; k < VL; k++) {
-                printf("%d ", matrix[i][j][k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-}
-
-Allocation generate_allocation(Vehicle v, Package* p, int pn) {
+float generate_allocation(Vehicle v, Package* p, int pn) {
     int WL = v.weight;
     int VL = v.volume;
-    int*** matrix = (int***)malloc((pn+1) * sizeof(int**));
-    for (int i = 0; i < pn; i++) {
-        matrix[i] = (int**)malloc((WL+1) * sizeof(int*));
-        for (int j = 0; j < WL; j++)
-            matrix[i][j] = (int*)calloc((VL+1), sizeof(int));
-    }
-    for(int i = 1; i < i <= pn+1; i++){
-        for(int w = 1; w <= WL+1; w++)
-            for(int v = 1; v <= VL+1; v++){
-                if (w - p[i].weight < 0 || v - p[i].volume < 0)
-                    matrix[i, w, v] = matrix[i-1, w, v];
-                else
-                    matrix[i, w, v] = max(matrix[i-1, w, v], matrix[i-1][WL - p[i].weight][VL - p[i].volume] + p[i].value);
-            }  
-    }
-    float total_value = matrix[pn+1][WL+1][VL+1];
-    for(int i = pn+1; i > 0; i--){
 
+    int*** matrix = (int***)malloc((pn + 1) * sizeof(int**));
+    for (int i = 0; i <= pn; i++) {
+        matrix[i] = (int**)malloc((WL + 1) * sizeof(int*));
+        for (int j = 0; j <= WL; j++)
+            matrix[i][j] = (int*)calloc((VL + 1), sizeof(int));
     }
-    print_allocation_matrix(matrix, pn, WL, VL);
+
+    for (int i = 1; i <= pn; i++) {
+        for (int w = 1; w <= WL; w++) {
+            for (int v = 1; v <= VL; v++) {
+                if (p[i - 1].weight <= w && p[i - 1].volume <= v) {
+                    matrix[i][w][v] = max(matrix[i - 1][w][v], matrix[i - 1][w - p[i - 1].weight][v - p[i - 1].volume] + p[i - 1].value);
+                } else {
+                    matrix[i][w][v] = matrix[i - 1][w][v];
+                }
+            }
+        }
+    }
+    float total = matrix[pn][WL][VL];
+    int j = WL, k = VL;
+    for (int i = pn; i > 0 && j > 0 && k > 0; i--) {
+        if ((matrix[i][j][k] != matrix[i - 1][j][k]) && p[i].allocated == 0) {
+            printf("Package %s\n", p[i - 1].code);
+            j -= p[i - 1].weight;
+            k -= p[i - 1].volume;
+            p[i].allocated = 1;
+        }
+    }
+
+    for (int i = 0; i <= pn; i++) {
+        for (int j = 0; j <= WL; j++)
+            free(matrix[i][j]);
+        free(matrix[i]);
+    }
+    free(matrix);
+    return total;
 }
 
 void generate_all_allocations(Vehicle* v, int vn, Package* p, int pn) {
     for (int i = 0; i < vn; i++) {
-        Allocation a = generate_allocation(v[i], p, pn);
+        float val = generate_allocation(v[i], p, pn);
+        printf("Allocations for Vehicle [%s] R$%f:\n", v[i].plate, val);
+        printf("\n");
     }
 }
 
 void run_tasks(FILE* input, FILE* output) {
-    int v_num = read_number(input);
+    int v_num, p_num;
+    fscanf(input, "%d", &v_num);
     Vehicle* vs = create_vehicle_arr(input, v_num);
-    int p_num = read_number(input);
+    fscanf(input, "%d", &p_num);
     Package* ps = create_package_arr(input, p_num);
     generate_all_allocations(vs, v_num, ps, p_num);
+    free(vs);
+    free(ps);
 }
 
 int main(int argc, char* argv[]) {
-    File file = openFiles(argv[1], argv[2]);
-    run_tasks(file.input, file.output);
-    fclose(file.input);
-    fclose(file.output);
+    if (argc != 3) {
+        printf("Usage: %s input_file output_file\n", argv[0]);
+        return 1;
+    }
+
+    FILE* input = fopen(argv[1], "r");
+    FILE* output = fopen(argv[2], "w");
+
+    if (!input || !output) {
+        printf("Error opening files.\n");
+        return 1;
+    }
+
+    run_tasks(input, output);
+
+    fclose(input);
+    fclose(output);
+
     return 0;
 }
