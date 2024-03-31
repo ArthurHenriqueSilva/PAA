0566d7c422f7a494a41fc71c320e3f8956e57143
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2024-03-31 00:33:45 -0300

    feat: start formating output

--- src/arthurlima_202000012551_transportadora.c
+++ src/arthurlima_202000012551_transportadora.c
@@ -15,6 +15,15 @@ typedef struct Package {
     int allocated;
 } Package;
 
+typedef struct Node {
+    Package* package;
+    struct Node* next;
+} Node;
+
+typedef struct LinkedList {
+    Node* head;
+} LinkedList;
+
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
@@ -35,7 +44,7 @@ Vehicle* create_vehicle_arr(FILE* input, int quantity) {
 Package* create_package(FILE* input) {
     Package* package = malloc(sizeof(Package));
     fscanf(input, "%s %f %d %d", package->code, &package->value, &package->weight, &package->volume);
-    package->allocated = 0;
+    package->allocated = 1;
     return package;
 }
 
@@ -46,9 +55,12 @@ Package* create_package_arr(FILE* input, int quantity) {
     return ps;
 }
 
-float generate_allocation(Vehicle v, Package* p, int pn) {
-    int WL = v.weight;
-    int VL = v.volume;
+float generate_allocation(Vehicle veiculo, Package* p, int pn) {
+    LinkedList allocated_list;
+    allocated_list.head = NULL;
+    
+    int WL = veiculo.weight;
+    int VL = veiculo.volume;
 
     int*** matrix = (int***)malloc((pn + 1) * sizeof(int**));
     for (int i = 0; i <= pn; i++) {
@@ -57,6 +69,8 @@ float generate_allocation(Vehicle v, Package* p, int pn) {
             matrix[i][j] = (int*)calloc((VL + 1), sizeof(int));
     }
 
+    int w_total = 0;
+    int v_total = 0;
     for (int i = 1; i <= pn; i++) {
         for (int w = 1; w <= WL; w++) {
             for (int v = 1; v <= VL; v++) {
@@ -69,29 +83,52 @@ float generate_allocation(Vehicle v, Package* p, int pn) {
         }
     }
     float total = matrix[pn][WL][VL];
+
     int j = WL, k = VL;
     for (int i = pn; i > 0 && j > 0 && k > 0; i--) {
-        if ((matrix[i][j][k] != matrix[i - 1][j][k]) && p[i].allocated == 0) {
-            printf("Package %s\n", p[i - 1].code);
+        if ((matrix[i][j][k] != matrix[i - 1][j][k]) && p[i - 1].allocated == 1) {
+            w_total += p[i - 1].weight;
+            v_total += p[i - 1].volume;
             j -= p[i - 1].weight;
             k -= p[i - 1].volume;
-            p[i].allocated = 1;
+            p[i - 1].allocated = 0;            
+            Node* new_node = malloc(sizeof(Node));
+            new_node->package = &p[i - 1];
+            new_node->next = allocated_list.head;
+            allocated_list.head = new_node;
         }
     }
 
+    printf("[%s] R$%.2f %dKg(%%) %dL(%%)\n", veiculo.plate, total, w_total, v_total);
+    Node* current = allocated_list.head;
+    while (current != NULL) {
+        printf("%s\n", current->package->code);
+        current = current->next;
+    }
+    
+    // Libera a memória alocada para a matriz
     for (int i = 0; i <= pn; i++) {
         for (int j = 0; j <= WL; j++)
             free(matrix[i][j]);
         free(matrix[i]);
     }
     free(matrix);
+    
+    // Libera a memória alocada para a lista encadeada
+    Node* temp;
+    while (allocated_list.head != NULL) {
+        temp = allocated_list.head;
+        allocated_list.head = allocated_list.head->next;
+        free(temp);
+    }
+    
     return total;
 }
+
 
 void generate_all_allocations(Vehicle* v, int vn, Package* p, int pn) {
     for (int i = 0; i < vn; i++) {
         float val = generate_allocation(v[i], p, pn);
-        printf("Allocations for Vehicle [%s] R$%f:\n", v[i].plate, val);
         printf("\n");
     }
 }
